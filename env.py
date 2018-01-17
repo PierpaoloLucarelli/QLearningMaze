578d26f48c7ebc02607de83d7e6514a308999a25
Pierpaolo <plucarelli.94@gmail.com>
2018-01-17 12:57:21 +0000

    added bad blocks

--- env.py
+++ env.py
@@ -36,6 +36,27 @@ class Maze(tk.Tk, object):
 			x0, y0, x1, y1 = c, 0, c, HEIGHT
 			self.canvas.create_line(x0, y0, x1, y1)
 
+		bad_origin = np.array([2*SCALE, 2*SCALE]);
+		# bad blocks
+
+		bad_center = bad_origin + np.array([SCALE/2, SCALE/2])
+		self.bad1 = self.canvas.create_rectangle(
+            bad_center[0] - SCALE/2, bad_center[1] - SCALE/2,
+            bad_center[0] + SCALE/2, bad_center[1] + SCALE/2,
+            fill='black')
+		bad_origin2 = np.array([3*SCALE, 2*SCALE]);
+		bad_center2 = bad_origin2 + np.array([SCALE/2, SCALE/2])
+		self.bad2 = self.canvas.create_rectangle(
+            bad_center2[0] - SCALE/2, bad_center2[1] - SCALE/2,
+            bad_center2[0] + SCALE/2, bad_center2[1] + SCALE/2,
+            fill='black')
+		bad_origin3 = np.array([1*SCALE, 3*SCALE]);
+		bad_center3 = bad_origin3 + np.array([SCALE/2, SCALE/2])
+		self.bad3 = self.canvas.create_rectangle(
+            bad_center3[0] - SCALE/2, bad_center3[1] - SCALE/2,
+            bad_center3[0] + SCALE/2, bad_center3[1] + SCALE/2,
+            fill='black')
+
 		self.canvas.pack()
 
 
