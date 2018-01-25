153f1be60fbc75a011698a465d2d121794ff6cdd
Pierpaolo <plucarelli.94@gmail.com>
2018-01-25 22:02:46 +0000

    added arg parser

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import argparse
 from maze_env import Maze
 from visualiser import Visualiser
 from rl_brain import QLearn
 import time
 
-TEST = False
 VIS = False
 N_EPISODES = 5000
-def test():
+def test(vis_):
 	start_time = time.time()
 	for episode in range(N_EPISODES):
 		# initial observation
@@ -14,7 +14,7 @@ def test():
 		if(episode % 500 == 0):
 			print(str(float(episode) / N_EPISODES * 100) + "%")
 		while True:
-			if(VIS):
+			if(vis_):
 				vis.update_canvas(env.actor, env.enemy)
 			# RL choose action based on observation
 			action = RL.choose_action(str(observation))
@@ -50,12 +50,19 @@ def run_optimal():
 	vis.destroy()
 
 if __name__ == '__main__':
-	if(TEST):
+	parser = argparse.ArgumentParser(description='QMaze options')
+	parser.add_argument('--test', dest='test', action='store_true',
+                   help='if you wish to re-train')
+	parser.add_argument('--vis', dest='vis', action='store_true',
+                   help='if you wish to see the GUI')
+	args = parser.parse_args()
+	print(args)
+	if(args.test):
 		env = Maze()
 		RL = QLearn(actions=list(range(env.n_actions)))
-		if(VIS):
+		if(args.vis):
 			vis = Visualiser(4,4,80, env.hell_blocks, env.goal, env.enemy)
-		test()
+		test(args.vis)
 	else:
 		env = Maze()
 		vis = Visualiser(4,4,80, env.hell_blocks, env.goal, env.enemy)
